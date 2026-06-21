ced048143de48d59a1aadd0be936194e6e735ba1
Anjali853 <anjalianjali06149@gmail.com>
2026-06-21 17:12:53 +0530

    Wishlist

--- Server/controllers/productController.js
+++ Server/controllers/productController.js
@@ -36,6 +36,116 @@ const createProduct = async (req, res) => {
 };
 
 
+
+// Get all products
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({
+      count: products.length,
+      products,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+
+
+// Get Single Product
+const getProductById = async (req, res) => {
+  try {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json(product);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+
+
+// Get Products By Category
+const getProductsByCategory = async (req, res) => {
+  try {
+
+    const category = req.params.category;
+
+    const products = await Product.find({
+      category,
+    });
+
+    if (products.length === 0) {
+      return res.status(404).json({
+        message: "No products found",
+      });
+    }
+
+    res.status(200).json({
+      count: products.length,
+      products,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+
+// Search Products
+const searchProducts = async (req, res) => {
+  try {
+    const keyword = req.query.keyword;
+
+    const products = await Product.find({
+      $or: [
+        {
+          name: {
+            $regex: keyword,
+            $options: "i",
+          },
+        },
+        {
+          category: {
+            $regex: keyword,
+            $options: "i",
+          },
+        },
+      ],
+    });
+
+    res.status(200).json({
+      count: products.length,
+      products,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   createProduct,
+  getProducts,
+  getProductById,
+  getProductsByCategory,
+  searchProducts,
 };
--- Server/routes/productRoutes.js
+++ Server/routes/productRoutes.js
@@ -3,11 +3,23 @@ const router = express.Router();
 
 const {
   createProduct,
+  getProducts,
+  getProductById,
+  getProductsByCategory,
+  searchProducts,
 } = require("../controllers/productController");
-
-
 // Create Product Route
 router.post("/add", createProduct);
 
+router.get("/", getProducts);
+
+// Search pehle
+router.get("/search", searchProducts);
+
+// Category uske baad
+router.get("/category/:category", getProductsByCategory);
+
+// ID hamesha last
+router.get("/:id", getProductById);
 
 module.exports = router;

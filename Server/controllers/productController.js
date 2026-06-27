0e07317ff73f7eba4d4dae9913bf91a3c2b67463
Anjali853 <anjalianjali06149@gmail.com>
2026-06-27 23:58:32 +0530

    Added admin product update API

--- Server/controllers/productController.js
+++ Server/controllers/productController.js
@@ -187,11 +187,42 @@ const updateProduct = async (req, res) => {
   }
 };
 
+
+
+const deleteProduct = async (req, res) => {
+  try {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({
+      message: "Product deleted successfully 🗑️",
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+};
+
+
+
 module.exports = {
-    createProduct,
-    getProducts,
-    getProductById,
-    searchProducts,
-    getProductsByCategory,
-    updateProduct
+  createProduct,
+  getProducts,
+  getProductById,
+  searchProducts,
+  getProductsByCategory,
+  updateProduct,
+  deleteProduct,
 };

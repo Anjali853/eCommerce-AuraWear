36bceeb374af08440640966ebf6997b47155d23e
Anjali853 <anjalianjali06149@gmail.com>
2026-06-27 16:00:09 +0530

    Update Product

--- Server/controllers/productController.js
+++ Server/controllers/productController.js
@@ -142,6 +142,51 @@ const searchProducts = async (req, res) => {
   }
 };
 
+
+// Update Product (Admin)
+const updateProduct = async (req, res) => {
+  try {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    product.name = req.body.name || product.name;
+    product.description =
+      req.body.description || product.description;
+
+    product.price =
+      req.body.price || product.price;
+
+    product.category =
+      req.body.category || product.category;
+
+    product.image =
+      req.body.image || product.image;
+
+    product.mood =
+      req.body.mood || product.mood;
+
+    const updatedProduct = await product.save();
+
+    res.status(200).json({
+      message: "Product updated successfully ✅",
+      product: updatedProduct,
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+};
+
 module.exports = {
   createProduct,
   getProducts,

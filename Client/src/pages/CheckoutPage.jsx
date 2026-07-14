5d316e7d98569e945ef7a7a420b025ff04c276d2
Anjali853 <anjalianjali06149@gmail.com>
2026-07-14 23:41:51 +0530

    Implement wishlist, cart, and initial checkout flow

--- Client/src/pages/CheckoutPage.jsx
+++ Client/src/pages/CheckoutPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
+import { createOrder } from "../services/orderService";
 
 const CheckoutPage = () => {
   const [paymentMethod, setPaymentMethod] = useState("cod");

fb3443328c2f54485f367fd6d5031c18b8b573ce
Anjali853 <anjalianjali06149@gmail.com>
2026-07-19 14:31:23 +0530

    routes

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -3,11 +3,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const {
-  signupUser,
-  loginUser,
-  getProfile,
-} = require("../controllers/authController");
+const {signupUser,loginUser,getProfile,updateProfile,} = require("../controllers/authController");
 
 // Signup Route
 router.post("/signup", signupUser);
@@ -20,7 +16,7 @@ router.post("/login", loginUser);
 // Get Profile Route
 router.get("/profile", protect, getProfile);
 // Update Profile Route
-// router.put("/profile", protect, updateProfile);
+ router.put("/profile", protect, updateProfile);
 
 
 module.exports = router;
